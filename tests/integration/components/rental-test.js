64031e1f672c42d104efbc27571126e59a517802
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-02 10:56:07 +0530

    rentals, rental-test and image attributes added

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -1,26 +1,35 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'emberlearn/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupRenderingTest } from 'emberlearn/tests/helpers';
+import { module, test } from 'qunit';
 
 module('Integration | Component | rental', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders information about rentals', async function (assert) {
     await render(hbs`<Rental />`);
 
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Rental>
-        template block text
-      </Rental>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('article div').hasClass('rentals');
+    assert.dom('article h5 strong').hasText('Grand Old Mansion');
+    assert
+      .dom(
+        'article .rentals .row .col-md-8 .card-body .details .owner-location .owner'
+      )
+      .includesText('Veruca Salt');
+    assert
+      .dom(
+        'article .rentals .row .col-md-8 .card-body .details .owner-location .location'
+      )
+      .includesText('San Francisco');
+    assert
+      .dom(
+        'article .rentals .row .col-md-8 .card-body .details .type-bedrooms .type'
+      )
+      .includesText('Standalone');
+    assert
+      .dom(
+        'article .rentals .row .col-md-8 .card-body .details .type-bedrooms .bedrooms'
+      )
+      .includesText('15');
   });
 });
